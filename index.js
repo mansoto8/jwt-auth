2189d59b5ede89b8a9f79fc8da7354a4ea0fbc78
Juan Manuel <juan.soto@blackboard.com>
2020-09-11 07:13:33 -0500

    Initialization of npm project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 8888;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);  
+  });
+
+app.get('/time', (req, res) => {
+  const time = (new Date()).toLocaleTimeString();
+  res.status(200).send(`The Time is ${time}`);
+});
+
+app.get("*", (req, res) => {
+    res.sendStatus(404);
+  });
+
+app.post("/login", (req, res) => {
+const user = req.body.username;
+res.status(200).send(`User's name is ${user}`);
+})
